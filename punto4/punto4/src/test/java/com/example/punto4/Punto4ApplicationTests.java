b77e64f4682fd2cfa7f1e10359300bcedc3b0885
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2024-09-18 15:02:44 -0500

    config

---
+++ punto4/punto4/src/test/java/com/example/punto4/Punto4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.punto4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Punto4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
