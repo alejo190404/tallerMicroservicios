2ea596dc7d9679f1c0dad51dfedeed79d50fd5ef
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2024-09-18 14:51:23 -0500

    gateway creado

---
+++ punto3/src/test/java/com/ms_taller/punto3/Punto3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_taller.punto3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Punto3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
